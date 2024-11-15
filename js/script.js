708dcb687628d49ab6d2e78192374531f3ae651b
Sergey Opekunov <sergos5@yandex.ru>
2024-11-15 18:37:31 +0300

    перемещение блоков вывел в отдельную функцию

--- js/script.js
+++ js/script.js
@@ -3,37 +3,57 @@
 const field = document.querySelector('.field')
 let chips = field.querySelectorAll('.chip')
 
+/* let direction = 'right'
+let indexEmptyBlock */
+
 const checkEmptyChip = (index) => {
     if (chips[index+1] && index%4 !=3 && chips[index+1].classList.contains('empty')) {
-        chips[index].before(chips[index+1])
-        chips = field.querySelectorAll('.chip')    
-        console.log('справа')
+        moveEmptyBlock(index+1, 'left')      
     }
     if (chips[index-1] && index%4 !=0 && chips[index-1].classList.contains('empty')) {
-        chips[index-1].before(chips[index])
-        chips = field.querySelectorAll('.chip') 
-        console.log('слева')
+        moveEmptyBlock(index-1, 'right')      
     }
-    if (chips[index+4] && chips[index+4].classList.contains('empty')) {        
-        chips[index].before(chips[index+4])
-        chips[index+3].after(chips[index])
-        chips = field.querySelectorAll('.chip')
-        console.log('снизу')
+    if (chips[index+4] && chips[index+4].classList.contains('empty')) {     
+        moveEmptyBlock(index+4, 'up')        
     }
     if (chips[index-4] && chips[index-4].classList.contains('empty')) {
-        chips[index-4].before(chips[index])
-        chips[index-1].after(chips[index-4])
-
-        chips = field.querySelectorAll('.chip')
-        console.log('сверху')
+        moveEmptyBlock(index-4, 'down')        
     }      
 }
+
+const moveEmptyBlock = (indexEmptyBlock, direction) => {
+    //console.log(indexEmptyBlock);
+    //console.log(direction);   
+    if (direction=='left') {
+        chips[indexEmptyBlock-1].before(chips[indexEmptyBlock])
+        console.log("empty влево")        
+    }
+    if (direction=='right') {
+        chips[indexEmptyBlock].before(chips[indexEmptyBlock+1])
+        console.log("empty вправо")        
+    }
+    if (direction=='up') {
+        chips[indexEmptyBlock-4].before(chips[indexEmptyBlock])
+        chips[indexEmptyBlock-1].after(chips[indexEmptyBlock-4])
+        console.log("empty вверх")        
+    }
+    if (direction=='down') {
+        chips[indexEmptyBlock].before(chips[indexEmptyBlock+4])
+        chips[indexEmptyBlock+3].after(chips[indexEmptyBlock])
+        console.log("empty вниз")        
+    }
+    chips = field.querySelectorAll('.chip')
+    if (chips[15].classList.contains('empty')) {
+        console.log('надо проверить решение');        
+    }
+}
+
 
 field.addEventListener('click', (e)=> { 
     if (e.target.classList.contains('chip'))  {    
         chips.forEach((item, index)=> {                      
             if (e.target == item) {
-                console.log(index);                
+                //console.log(index);                
                 checkEmptyChip(index)
             }
         })        
